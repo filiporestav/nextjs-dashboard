be1b96aec89475a664f11bf772043e6118260b11
Filip <orestavfilip@gmail.com>
2025-01-30 14:02:25 +0100

    finish course

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,6 +1,11 @@
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
 
 import { Suspense } from "react";
 import {
